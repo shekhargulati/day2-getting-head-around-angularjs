4cf5957cf903dd455cfc65d128dd675910ba9857
Shekhar Gulati <shekhargulati84@gmail.com>
2013-10-30 00:17:22 +0530

    Wrote book controller

---
+++ app.js
@@ -0,0 +1,8 @@
+function BookCtrl($scope){
+	$scope.books = [
+		{'name': 'Effective Java', 'author':'Joshua Bloch'},
+		{'name': 'Year without Pants', 'author':'Scott Berkun'},
+		{ 'name':'Confessions of public speaker','author':'Scott Berkun'},
+		{'name':'JavaScript Good Parts','author':'Douglas Crockford'}
+	]
+}
